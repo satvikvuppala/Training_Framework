9bfae456ef2b25d39527ff2cffae6d033bd599f7
satvikvuppala <vuppala.satvik@genzsoln.com>
2023-10-16 18:59:36 +0530

    16/10

--- lessons/relationalOperators.js
+++ lessons/relationalOperators.js
@@ -1,8 +1,8 @@
 var x = '1'
-console.log ( x == '1' )
-console.log ( x == 1 )
-console.log ( x === '1' )
-console.log ( x === 1 )
+console.log ( x == '1' )        // Comparing only values
+console.log ( x == 1 )          // Comparing only values
+console.log ( x === '1' )       // Comparing values and datatype
+console.log ( x === 1 )         // Comparing values and datatype
 
 var a, b, c, d, e
 a = 10 > 5
